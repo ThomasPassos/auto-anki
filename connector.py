363919bf11755d0a5da0d7413f493eab8590cf51
Thomas Passos <tm123br@live.com>
2023-08-05 14:21:13 -0300

    Refatoração
    Refatoração da classe responsável pela conexão com a API e criação da dataclass Word, para uso do conector.

--- connector.py
+++ connector.py
@@ -1,67 +1,84 @@
+from dataclasses import dataclass, field
+from typing import List, Tuple
+
 import requests
+
+
+# Dataclass que simula uma palavra
+@dataclass
+class Word:
+    word: str
+    word_class: str
+    phonetic: str = ""
+    meanings: List[Tuple[str, str]] = field(default_factory=list)
 
 
 # Classe responsável por se conectar e puxar os dados da API:
 class DictionaryConnector:
-    __dictionary_path = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+    data_model = List[Tuple[str, str]]
     json_list = []
-    word_dicts_list = []
+    words = []
+    __dictionary_path = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 
-    def get_json_list(self, word_list: list):
+    def _generate_word_object(self, word_list: data_model):
         """
-        Receive a list with tuples of lenght 2, and update the json
-        of the data about them in the dict API.
+        Receive a data_model and create Word objects with the data.
         """
-        for word in word_list:
-            word = word[0]
-            word_json = requests.get(self.__dictionary_path + word).json()
-            self.json_list.append(word_json)
+        for word_tuple in word_list:
+            word = Word(word_tuple[0], word_tuple[1])
+            self.words.append(word)
 
-    def get_basic_word_data(self):
+    def get_json_list(self):
         """
-        Receive a list of jsons obtained by the function "get_json_list" and
-        update word_dicts_list property with the word and its phonetic within a list.
+        Get the json with word information by the Free Dictionary API.
         """
-        for json in self.json_list:
-            word = json[0]["word"]
-            phonetic = json[0]["phonetic"]
-            self.word_dicts_list.append({"word": word, "phonetic": phonetic})
+        for word_object in self.words:
+            word = word_object.word
+            # TODO - Teste
+            try:
+                request = requests.get(self.__dictionary_path + word)
+                request.raise_for_status()
+            except requests.exceptions.HTTPError:
+                word_json = {"error_4XX": "word not found"}
+                self.json_list.append(word_json)
+            else:
+                word_json = request.json()
+                self.json_list.append(word_json)
 
-    def get_word_meaning(self, word_list: list):
+    # TODO - Teste
+    def get_word_atributes(self):
         """
-        Receive a word_list and update the word_dicts_list with
-        a list of tuples with meanings and examples.
+        Update the Word object atributes phonetic and meanings,
+        the latter with a tuple containing meaning and example.
         """
-        meanings = []
-        for iterator, json in enumerate(self.json_list):
-            for word in json:
-                for meaning_item in word["meanings"]:
-                    gramatical_class = meaning_item["partOfSpeech"]
-                    if gramatical_class == word_list[iterator][1]:
-                        for definition in meaning_item["definitions"]:
-                            meaning = definition["definition"]
-                            if "example" in definition:
-                                example = definition["example"]
-                            else:
-                                example = "without example"
-                            meanings.append((meaning, example))
-            self.word_dicts_list[iterator]["meaning-example"] = meanings
+        for word, json in zip(self.words, self.json_list):
+            if "error_4XX" not in json:
+                phonetic = json[0]["phonetic"]
+                word.phonetic = phonetic
+                for word_dict in json:
+                    for meaning_item in word_dict["meanings"]:
+                        gramatical_class = meaning_item["partOfSpeech"]
+                        if gramatical_class == word.word_class:
+                            for definition in meaning_item["definitions"]:
+                                meaning = definition["definition"]
+                                if "example" in definition:
+                                    example = definition["example"]
+                                else:
+                                    example = "without example"
+                                word.meanings.append((meaning, example))
 
-    def get_words_list(self, word_list: list) -> list:
+    def get_words_list(self, word_list: data_model) -> List[Word]:
         """
-        Receive a list of tuples with word and gramatical class per tuple, and
-        return a list of dicts including word meanings and its examples, and
-        phonetic of the word.
+        Receive a data_model with word and word class, and
+        return a list of Word objects.
 
         Example:
             params:
-                word_list: [("water, noun")]
+                word_list: [("water", "noun"), ...]
             return:
-                dict: {"word": "water",
-                       "phonetic": "/ˈwɔːtər/",
-                       "meaning-example": [(definition, example)]}.
+                words: [Word(word="water", word_class="noun", ...), ...].
         """
-        self.get_json_list(word_list)
-        self.get_basic_word_data()
-        self.get_word_meaning(word_list)
-        return self.word_dicts_list
+        self._generate_word_object(word_list)
+        self.get_json_list()
+        self.get_word_atributes()
+        return self.words
